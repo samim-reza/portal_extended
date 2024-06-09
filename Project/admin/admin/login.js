76db0cabbf9e2563ed3c8ac37e34e24173123f07
samim-reza <saamiimreza@gmail.com>
2024-06-09 15:31:36 +0600

    complete

---
+++ Project/admin/admin/login.js
@@ -0,0 +1,32 @@
+function validateForm() {
+    console.log("Validating form...");
+    var studentid = document.getElementById("student-id").value;
+    var password = document.getElementById("password").value;
+
+    if (studentid === "" || password === "") {
+        alert("ID and password are required.");
+        return false;
+    } else {
+        //  var link = 'portal.html?studentid=' + studentid;
+        //     $('.login').attr('href', link),
+        $.ajax({
+            type: "POST",
+            url: "login.php",
+            data: {
+                id: studentid,
+                password: password
+            },
+            success: function (response) {
+                //alert(response);
+                if (response === "success") {
+                    window.location.href = `admin.html`;
+                } else {
+                    alert("Invalid ID or password.");
+                }
+            },
+            error: function (xhr, status, error) {
+                console.error(xhr.responseText);
+            }
+        });
+    }
+}
